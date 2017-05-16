12996463de867b9d95a4f9231d14d3e044708851
rodrigoperissini <rodrigo.cesar.perissini@gmail.com>
2017-05-16 17:36:25 -0300

    added a new e2e test

--- tests/e2e-tests.js
+++ tests/e2e-tests.js
@@ -1,28 +1,33 @@
 // spec.js
 
-describe('Check aplication title', function() {
-  it('check page title', function() {
-    browser.get('http://localhost:3000/clientes');
-    var something = element(by.tagName('h1'));
-    expect(something.getText()).toBe("Lista de Clientes");
-  });
+describe('Check aplication title', function () {
+    it('check page title', function () {
+        browser.get('http://localhost:3000/clientes');
+        var something = element(by.tagName('h1'));
+        expect(something.getText()).toBe("Lista de Clientes");
+    });
 });
 
-describe('Check home aplication title', function() {
-  it('should have a title', function() {
-    browser.driver.get('http://localhost:3000/');
-    var something = browser.driver.findElement(by.tagName('body'));
-    expect(something.getText()).toEqual("batata");
-  });
-  });
-
- describe('Check if the list is correct', function() {
-    it('check 10th name in the list', function() {
-       browser.get('http://localhost:3000/clientes');
-       var something = element.all(by.repeater('cliente in clientes'));
-       expect(something.count()).toBe(150);
+describe('Check home aplication title', function () {
+    it('should have a title', function () {
+        browser.driver.get('http://localhost:3000/');
+        var something = browser.driver.findElement(by.tagName('body'));
+        expect(something.getText()).toEqual("batata");
+    });
 });
-  
 
+describe('Check if the list is correct', function () {
+
+    var something = element.all(by.repeater('cliente in clientes'));
+
+    it('check 10th name in the list', function () {
+        browser.get('http://localhost:3000/clientes');
+        expect(something.count()).toBe(150);
+    });
+
+    it('check if the last name in the list is "Teste 150"', function () {
+        browser.get('http://localhost:3000/clientes');
+        expect(something.last().getText()).toContain('Teste 150');
+    });
 
 });
